26b99b5ea71311c134081b370ced64fad20bfdf9
AntonelaDiPinto <dipintoantonela@gmail.com>
2025-05-26 19:36:27 -0300

    Modificaciones realizadas en la clase del viernes 23 de Mayo

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,41 @@
 import { Injectable } from '@nestjs/common';
 
-@Injectable()
+/*@Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hola a Todos y Todas!';
+  }
+}
+*/
+
+export interface iTrack {
+  id: number;
+  title: string;
+  duration: number;
+  artist: string;
+}
+
+export const tracks: iTrack[] = [
+  {id: 1,
+    title: "Canción 1",
+    duration: 120,
+    artist: "Interprete 1"
+  },
+  {id: 2,
+    title: "Canción 2",
+    duration: 80,
+    artist: "Interprete 2"
+  },
+  {id: 3,
+    title: "Canción 3",
+    duration: 95,
+    artist: "Interprete 3"
+  }
+];
+
+@Injectable()
+export class AppService {
+  getTracks():any {
+    return tracks;
   }
 }
