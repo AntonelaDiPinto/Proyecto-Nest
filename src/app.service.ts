e547f3baa9b91c2a9d39ff2a48cd25dcc1e27437
AntonelaDiPinto <dipintoantonela@gmail.com>
2025-06-14 10:58:43 -0300

    Se completan los diferentes archivos con el repositorio compartido en clase

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,41 +1,38 @@
 import { Injectable } from '@nestjs/common';
 
-/*@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hola a Todos y Todas!';
-  }
-}
-*/
-
-export interface iTrack {
-  id: number;
-  title: string;
-  duration: number;
-  artist: string;
+/*export interface iTrack {
+  id: number,
+  title: string,
+  duration: number,
+  artist: string
 }
 
-export const tracks: iTrack[] = [
-  {id: 1,
-    title: "Canción 1",
+export const tracks: iTrack[] = [ //no es lo de ideal, es solo de prueba
+  {
+    id: 1,
+    title: "cancion 1",
     duration: 120,
-    artist: "Interprete 1"
+    artist: "interprete 1",
   },
-  {id: 2,
-    title: "Canción 2",
-    duration: 80,
-    artist: "Interprete 2"
+   {
+    id: 2,
+    title: "cancion 2",
+    duration: 130,
+    artist: "interprete 2"
   },
-  {id: 3,
-    title: "Canción 3",
-    duration: 95,
-    artist: "Interprete 3"
+   {
+    id: 3,
+    title: "cancion 2",
+    duration: 220,
+    artist: "interprete 3"
   }
-];
+]*/
 
 @Injectable()
 export class AppService {
-  getTracks():any {
-    return tracks;
+  getTracks(): string {
+    return "hola";
   }
+
+
 }
---
+++ src/track/track.service.ts
@@ -0,0 +1,91 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Track } from './track.interface';
+
+const BASE_URL = 'http://localhost:3030/tracks/';
+@Injectable()
+export class TrackService {
+
+  async updateTrackById(id: number, body: Track): Promise<Track | undefined> {
+
+    const isTrack: Track | undefined = await this.getTrackById(id);
+
+    if (!isTrack || !Object.keys(isTrack).length) {
+      console.warn(`El track de id ${id} no existe`)
+      return;
+    }
+
+    const updateTrack = { ...body, id };
+    console.log('Pista actualizada', updateTrack.title);
+
+    const res = await fetch(BASE_URL + id, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updateTrack),
+    });
+
+    if (!res.ok) {
+      console.log('no se pudo actualizar')
+      return;
+    }
+    // const parsed = await res.json();
+    // return parsed
+
+  }
+
+  async deleteTrackById(id: number): Promise<Track> {
+    const res = await fetch(BASE_URL + id, {
+      method: 'DELETE',
+    });
+    const parsed = res.json();
+    return parsed;
+  }
+
+  async createTrack(track: Track): Promise<Track> {
+    const idn = await this.setId();
+    //const newTrack={id,...track};
+    const newTrack: Track = {
+      id: idn,
+      title: track.title,
+      duration: track.duration,
+      artist: track.artist,
+    };
+    const res = await fetch(BASE_URL, {
+      method: 'POST',
+      body: JSON.stringify(newTrack),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const parsed = await res.json();
+    return parsed;
+  }
+
+  private async setId(): Promise<number> {
+    const tracks = await this.getTracks();
+    const id: number = tracks[tracks.length - 1].id + 1;
+    //=tracks[2]
+    return id; //4
+  }
+
+  async getTrackById(id: number): Promise<Track | undefined> {
+    const res = await fetch(BASE_URL + id);
+
+    try {
+      const parsed = await res.json();
+      if (Object.keys(parsed).length) return parsed;
+
+    } catch (err) {
+      console.log(err)
+      throw new NotFoundException('no se encontro el recurso');
+    }
+  }
+
+  async getTracks(): Promise<Track[]> {
+    const res = await fetch(BASE_URL);
+    const parsed = await res.json();
+    return parsed;
+  }
+
+}
